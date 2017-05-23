927df6bbf7fbe6c79a357a59740ec1589dd34c5c
su11003333 <su11003333@gmail.com>
2017-05-24 01:05:02 +0800

    before persisting data

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,8 +19,8 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index',compact('users'));
     }
 
     /**
@@ -28,7 +31,9 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+
+      $roles = Role::lists('name','id')->all();
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -37,9 +42,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+
+        return $request->all();
     }
 
     /**
